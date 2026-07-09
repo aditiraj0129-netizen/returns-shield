54709ff06737909756e625a869dc3cace45115e3
aditiraj0129-netizen <aditiraj0129@gmail.com>
2026-07-09 21:32:12 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:8000' })
+const api = axios.create({ baseURL: 'https://returns-shield-2.onrender.com' })
 
 export const getStats = () => api.get('/returns/stats')
 export const getAllReturns = () => api.get('/returns/all')
